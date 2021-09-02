aaba55154981b346d895a77c2356bef9272257cf
yatinbawa1 <yatinabc3@gmail.com>
2021-09-02 07:53:02 +0530

    Body Parser Modified

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
-const express = require('express');
+const express = require("express");
 
 // Routes are exported from them
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-//Controller  
+//Controller
 const errorController = require("./controllers/error");
 
 // Body Parser to parse the body of requests
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // Making a Express App
 const app = express();
 
 // Setting Something for html templating
-app.set('view engine','pug');
-app.set('views','views');
+app.set("view engine", "pug");
+app.set("views", "views");
 
 // This tells about public path for style and JS files
 app.use(express.static("./public"));
 // This makes use of middleware that parse whole body
-app.use(bodyParser.urlencoded());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 // This Middlewares helps in routing
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-// This is when path is invalid 
+// This is when path is invalid
 app.use(errorController.get404);
 
 // Listens for requests to server;
